696375db44cce7d38e58cfd51f537a4ac585f1c0
Ian Torquato <iantorquato2@gmail.com>
2021-07-27 20:03:03 -0300

    Componentizing Input and rename page Home -> SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+import { Button, Flex, Stack } from '@chakra-ui/react';
 
-export default function Home() {
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex as="form" width="100%" maxW={360} bg="gray.800" p={8} borderRadius={8} flexDir="column">
         <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input id="email" name="email" type="email" focusBorderColor="blue.500" bg="gray.900" variant="filled" _hover={{ bgColor: 'gray.900' }} size="lg"/>
-          </FormControl>
+          <Input type="email" name="email" label="E-mail" />
 
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input id="password" name="password" type="password" focusBorderColor="blue.500" bg="gray.900" variant="filled" _hover={{ bgColor: 'gray.900' }} size="lg"/>
-          </FormControl>
+          <Input type="password" name="password" label="Senha" />
         </Stack>
 
         <Button type="submit" mt={6} colorScheme="blue" size="lg">Entrar</Button>
