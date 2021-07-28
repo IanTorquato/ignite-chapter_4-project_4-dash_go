637a8d60282f1ed7dd0c00a96c1865191f6f0c83
Ian Torquato <iantorquato2@gmail.com>
2021-07-27 22:17:57 -0300

    Adding basic Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,16 @@
+import { Flex } from '@chakra-ui/react';
+
 import { Header } from '@dashgo/components/Header';
+import { Sidebar } from '@dashgo/components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Header />
-    </>
+
+      <Flex w="100%" my="6" maxW={1400} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
