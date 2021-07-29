3dedf6f003dac0332965c36c3bc80a3ac2e3c0ac
Ian Torquato <iantorquato2@gmail.com>
2021-07-28 22:11:33 -0300

    Making Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+type ProfileProps = {
+  showProfileData?: boolean;
+};
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text> Ian Torquato </Text>
-        <Text color="gray.300" fontSize="small">
-          iantorquato2@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text> Ian Torquato </Text>
+          <Text color="gray.300" fontSize="small">
+            iantorquato2@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Ian Torquato" src="https://github.com/iantorquato.png" />
     </Flex>
