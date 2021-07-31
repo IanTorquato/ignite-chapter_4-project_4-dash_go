851b35e4e06c12c7b0c200fada37cc36dc6bd78f
Ian Torquato <iantorquato2@gmail.com>
2021-07-30 23:08:51 -0300

    Starting paging

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable camelcase */
 import faker from 'faker';
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
   name: string;
@@ -31,15 +31,32 @@ export function makeServer() {
 
     // eslint-disable-next-line @typescript-eslint/no-shadow
     seeds(server) {
-      server.createList('user', 16);
+      server.createList('user', 200);
     },
 
     routes() {
       this.namespace = 'api';
       this.timing = 750;
-      this.get('/users');
+
+      this.get('/users', (schema, request) => {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = schema.all('user').models.slice(pageStart, pageEnd);
+
+        // console.log(users.models);
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
+
       this.post('/users');
+
       this.namespace = '';
+
       this.passthrough();
     },
   });
